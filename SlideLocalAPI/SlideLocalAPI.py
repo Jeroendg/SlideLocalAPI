dbc160ddc9e12a3065c98418c3bbc89111c036fe
Jeroen <Jeroendg@users.noreply.github.com>
2024-05-07 23:06:06 +0200

    Revert "Update SlideLocalAPI.py"
    This reverts commit 93a5f5c081a4f82de94f574bd2b1bb6796092ded.

--- SlideLocalAPI/SlideLocalAPI.py
+++ SlideLocalAPI/SlideLocalAPI.py
@@ -1,15 +1,28 @@
+import aiohttp
+import asyncio
 import json
 import logging
-import urllib
-from urllib import request, parse
 
 _LOGGER = logging.getLogger(__name__)
 
 URI_PREFIX = "/rpc/Slide."
 DEFAULT_TIMEOUT = 10
+POS_MIN = 0
+POS_MAX = 1
+
+class ClientConnectionError(Exception):
+    """Error to indicate to connection issues with the Slide API."""
+
+    pass
+
+
+class ClientTimeoutError(Exception):
+    """Error to indicate to timeout issues with the Slide API."""
+
+    pass
 
 class SlideLocal:
-	#Class for Slide devices
+	"""Class for Slide devices"""
 
 	def __init__(self, host, id, timeout=DEFAULT_TIMEOUT, prefix=URI_PREFIX):
 		self._host = host
@@ -18,104 +31,143 @@ class SlideLocal:
 		self._prefix = prefix
 		self._cnoncecount = 0
 		self._requestcount = 0
-		self._slides = {}
 
-	async def _make_request(self, resource):
-		#Handle HTTP requests
+	async def _request(self, uri, data=None):
+		"""Handle HTTP requests"""
 
+		# Increment request counter
 		self._requestcount += 1
 		if self._requestcount > 99999:
 			self._requestcount = 1
 
-		_LOGGER.debug(
-            "REQ-L%d: data=%s",
-            self._requestcount,
-            resource,
-        )
-
-		response_code = resource.getcode()
-
-		if response_code == 200:
-			try:
-				response = resource.read().decode('utf-8')
-
-				try:
-					json_response = json.loads(response)
-				except json.decoder.JSONDecodeError:
-					_LOGGER.error("RES-L%d: INVALID JSON=%s", self._requestcount, response)
-					json_response = None
-
-				return response_code, json_response
-
-			except urllib.error.HTTPError:
-				_LOGGER.error("RES-L%d: HTTP Error=%s", self._requestcount, response)
-				json_response = None
-
-		else:
-			_LOGGER.error("RES-L%d: HTTPCode=%s, Data=%s", self._requestcount, response_code)
-			return response_code, None
-
-	async def _request(self, uri, data=None):
+		# Build request URL and set timeout
 		url = "http://" + self._host + self._prefix + uri
 		timeout = self._timeout
-		resource = urllib.request.urlopen(url, data, timeout)
-		response_code, response = await self._make_request(resource)
-		return response
+
+		# Request data from Slide
+		try:
+			async with aiohttp.request("POST", url, data=data) as response:
+				# Check if request was succesful
+				if response.status == 200:
+					response_data = await response.text()
+					# Log succesful request
+					_LOGGER.debug(
+						"RES-C%d: URL=%s, HTTPCode=%s, Data=%s",
+						self._requestcount,
+						url,
+						response.status,
+						response_data,
+						)
+
+					# Decode data from request
+					try:
+						json_data = json.loads(response_data)
+					except json.decoder.JSONDecodeError:
+						# Log invalid JSON error
+						_LOGGER.error(
+							"RES-L%d: URL=%s, INVALID JSON=%s",
+							self._requestcount,
+							url,
+							response_data,
+						)
+						json_data = None
+
+					return json_data
+				# Error 401, API version on slide is possibly out of date or Local API is not active
+				elif response.status == 401:
+					response_data = await response.text()
+					# Log error 401
+					_LOGGER.error(
+						"RES-L%d: URL=%s, HTTPCode=%s, Data=%s",
+						self._requestcount,
+						url,
+						response.status,
+						response_data,
+					)
+					_LOGGER.error(
+						"Make sure the Local API is active on your Slide and up-to-date. Contact Slide support if your device needs updating."
+					)
+				# Log all other response codes
+				else:
+					response_data = await response.text()
+					_LOGGER.error(
+						"RES-L%d: URL=%s, HTTPCode=%s, Data=%s",
+						self._requestcount,
+						url,
+						response.status,
+						response_data,
+					)
+
+				return None
+		except (
+			aiohttp.client_exceptions.ClientConnectionError,
+			aiohttp.client_exceptions.ClientConnectorError,
+		) as err:
+			raise ClientConnectionError(str(err)) from None
+		except asyncio.TimeoutError as err:
+			raise ClientTimeoutError("Connection Timeout") from None
+
 
 	async def slide_get_info(self):
-		#Fetch Slide information
+		"""Fetch the Slide information."""
 		result = await self._request("GetInfo")
 		return result
 
 	async def slide_get_pos(self):
-		#Get Slide Position
+		"""Get the current position of the Slide."""
 		result = await self.slide_get_info()
+
+		# Check if there is a valid position in the data
 		if result:
 			if "pos" in result:
 				return result["pos"]
-
-			_LOGGER.error("SlideGetPosition: Missing key 'pos' in JSON=%s", json.dumps(result))
-		return None
+			else:
+				# Log position error
+				_LOGGER.error(
+					"SlideGetPosition: Missing key 'pos' in JSON=%s",
+					json.dumps(result),
+				)
+				return None
 
 	async def slide_set_pos(self, pos):
-		#Set Slide position
+		"""Set a new position on the Slide."""
+
+		# Verify that position is numeric
 		try:
 			set_pos = float(pos)
 		except ValueError:
-			_LOGGER.error("SlideSetPosition: '%s' has to be numeric", pos)
+			_LOGGER.error(
+				"SlideSetPosition: '%s' has to be numeric",
+				pos,
+			)
 			return False
 
-		if set_pos < 0:
-			set_pos = 0
-		if set_pos > 1:
-			set_pos = 1
+		# Make sure the position is within bounds, correct if necessary
+		set_pos = max(POS_MIN, min(POS_MAX, set_pos))
 
-		str_pos = str(set_pos)
+		# Build the data for the request
+		data = json.dumps({"pos": set_pos}).encode('utf-8')
 
-		data = bytes("{\"pos\": " + str_pos + "}", 'utf-8')
-
+		# Execute the request
 		response = await self._request("SetPos", data)
 		return bool(response)
 
 	async def slide_open(self):
-		#Open Slide
+		"""Open the Slide"""
 		response = await self.slide_set_pos(0)
 		return bool(response)
 
 	async def slide_close(self):
-		#Close Slide
+		"""Close the Slide"""
 		response = await self.slide_set_pos(1)
 		return bool(response)
 
 	async def slide_stop(self):
-		#Stop Slide
+		"""Stop the Slide immediately"""
 		response = await self._request("Stop")
 		return bool(response)
 
 	async def  slide_calibrate(self):
-		#Get starting position
-		#start = self.slide_get_pos()
-
-		#Calibrate Slide
+		"""Calibrate the slide"""
 		response = await self._request("Calibrate")
 		return bool(response)
