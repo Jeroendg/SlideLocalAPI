4bd1d26614a2e782cfc97b85e6eb7af294b1ba9a
Jeroen <Jeroendg@users.noreply.github.com>
2024-05-07 22:21:09 +0200

    Remove sensor domain

--- SlideLocalAPI/__init__.py
+++ SlideLocalAPI/__init__.py
@@ -12,7 +12,7 @@ from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
-PLATFORMS: list[str] = ["cover", "sensor"]
+PLATFORMS: list[str] = ["cover"]
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Slide Local from a config entry."""
